5fc9fbe2069ad7213ece68c8856542282eae3bed
User <user@gmail.com>
2018-08-09 17:26:33 -0300

    autoupdate`date +T`

--- commit_push.bat
+++ commit_push.bat
@@ -1,3 +1,3 @@
 git commit -a -m "autoupdate`date +%F-%T`"
-git push https://github.com/pablohernan/proposta_de_locacao_print
+git push https://github.com/pablohernan/proposta_de_mall_e_midia_print
 
