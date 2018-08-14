1fe7e2f2559c04247fb417a16e4645564c6e1634
User <user@gmail.com>
2018-08-13 21:05:43 -0300

    autoupdate`date +T`

--- public/js/popup.js
+++ public/js/popup.js
@@ -45,36 +45,70 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 function init(card,p){
 
-var data = new Date().toJSON().slice(0,10);
-var dataFormat = data.split('-')[2] + '.' + data.split('-')[1] + '.' + data.split('-')[0];
-$('#data_emissao').html(dataFormat);
-db_get_fields(card.internalId, function(ret){
+  var data = new Date().toJSON().slice(0,10);
+  var dataFormat = data.split('-')[2] + '.' + data.split('-')[1] + '.' + data.split('-')[0];
+  $('#data_emissao').html(dataFormat);
+  db_get_fields(card.internalId, function(ret){
 
-  var data = ret.data;
-  var arrFields = data.card.fields;
-  var id = ret.data.card.id;
+    var data = ret.data;
+    var arrFields = data.card.fields;
+    var id = ret.data.card.id;
 
-  for(var i = 0 ; i<arrFields.length ; i++){
+    for(var i = 0 ; i<arrFields.length ; i++){
 
-    if($("[id='"+arrFields[i].name+"']").length > 0 )
-      $("[id='"+arrFields[i].name+"']").val(arrFields[i].value);
+      if($("[id='"+arrFields[i].name+"']").length > 0 )
+        $("[id='"+arrFields[i].name+"']").val(arrFields[i].value);
 
-  }
-
-
-  // id
-  $("[id='id']").val(id);
-
-  //Endereço
-  var EnderecoFormat = db_get_field('Logradouro',data) + ', ' + db_get_field('Número',data) + ', ' + db_get_field('Complemento',data);
-  $("[id='Endereço']").val(EnderecoFormat); 
+    }
 
 
-  //Acrescentar Parecer - Superintendente Comercial.Superintendente Comercial
-  var supComercial = db_get_field('Superintendente Comercial',data,'Acrescentar Parecer - Superintendente Comercial');
-  $("[id='Superintendente Comercial']").val(supComercial); 
+    // id
+    $("[id='id']").val(id);
+
+    //Endereço
+    var EnderecoFormat = db_get_field('Logradouro',data) + ', ' + db_get_field('Número',data) + ', ' + db_get_field('Complemento',data);
+    $("[id='Endereço']").val(EnderecoFormat); 
 
 
-})
+    //Acrescentar Parecer - Superintendente Comercial.Superintendente Comercial
+    var supComercial = db_get_field('Superintendente Comercial',data,'Acrescentar Parecer - Superintendente Comercial');
+    $("[id='Superintendente Comercial']").val(supComercial); 
+
+
+  })
 
 }
+
+function printContainer(){
+
+  $('#container').removeClass( 'container_scroll' );
+  $('#btnSalvar').hide();
+
+  window.print();
+
+  $('#container').addClass( 'container_scroll' );
+  $('#btnSalvar').show();
+
+}
+
+
+function debug(){
+
+  $('input').each(function(){
+      $(this).val('[--'+$(this).attr('id')+'--]');
+  })
+
+  $('textarea').each(function(){
+      $(this).val('[--'+$(this).attr('id')+'--]');
+  })
+
+  $('select').each(function(){
+      $(this).replaceWith('<input type="text" tabindex="0" class="pp-input save" value="[--'+$(this).attr('id')+'--]" id="'+$(this).attr('id')+'" name="'+$(this).attr('id')+'">');
+  })
+
+  $('.pp-custom-select').removeClass('pp-custom-select');
+
+}
+
+  
+
