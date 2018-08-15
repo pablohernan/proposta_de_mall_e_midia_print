0e23b434fa06711084c37999e25a37b5a0cb9c47
User <user@gmail.com>
2018-08-15 17:01:43 -0300

    autoupdate`date +T`

--- public/js/popup.js
+++ public/js/popup.js
@@ -67,9 +67,22 @@ function init(card,p){
 
     $("[id='id']").val(id);
 
-    //Endereço
-    var EnderecoFormat = db_get_field('Logradouro',data) + ', ' + db_get_field('Número',data) + ', ' + db_get_field('Complemento',data);
-    $("[id='Endereço']").val(EnderecoFormat);     
+    //compuesto_Logradouro_Número_Complemento
+    var endFormat = db_get_field('Logradouro',data) + ', ' + db_get_field('Número',data) + ', ' + db_get_field('Complemento',data);
+    $("[id='compuesto_Logradouro_Número_Complemento']").val(endFormat);  
+
+    //compuesto_Início da Operação_Observações
+    var inicioFormat = db_get_field('Início da Operação',data) + '\\n ' + db_get_field('Observações',data);
+    $("[id='compuesto_Início da Operação_Observações']").val(inicioFormat); 
+
+    //compuesto_Valor do Aluguel_Observações sobre o valor do aluguel    
+    var aluguelFormat = db_get_field('Valor do Aluguel',data) + '\\n ' + db_get_field('Observações sobre o valor do aluguel',data);
+    $("[id='compuesto_Valor do Aluguel_Observações sobre o valor do aluguel']").val(aluguelFormat);         
+
+    //compuesto_Valor da Energia_Observações sobre o valor da energia
+    var energiaFormat = db_get_field('Valor da Energia',data) + '\\n ' + db_get_field('Observações sobre o valor da energia',data);
+    $("[id='compuesto_Valor da Energia_Observações sobre o valor da energia']").val(energiaFormat);         
+
 
   })
 
